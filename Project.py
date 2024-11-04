3b4bca51fffd170f942172a7a853ab94e3931e07
Alieu-K <alieu2567@gmail.com>
2024-11-04 11:50:09 -0500

    Changed the numbers that represented the ship sizes into words

--- Project.py
+++ Project.py
@@ -1,23 +1,22 @@
 
-# 1 - Small Ships, 2 - Medium Ships, 3 - Large Ships
 # Structured by ship size, Time (military), occupied status, name of ship/bay
-ship_bay_1 = [[1, (19, 22), 'Empty', 'Alpha'], 
-              [1, (13, 15), 'Occupied', 'Beta'], 
-              [3, (3, 6), 'Empty', 'Gamma'], 
-              [2, (6, 12), 'Empty', 'Delta'],
-              [3, (18, 24), 'Occupied', 'Epsilon'],
-              [3, (11, 18), 'Empty', 'Eta'],
-              [2, (7, 20), 'Empty', 'Theta'],
-              [1, (10, 14), 'Occupied', 'Iota'],
-              [2, (0, 9), 'Empty', 'Kappa'],
-              [3, (14, 19), 'Empty', 'Lambda']]
+ship_bay_1 = [['Small', (19, 22), 'Empty', 'Alpha'], 
+              ['Small', (13, 15), 'Occupied', 'Beta'], 
+              ['Big', (3, 6), 'Empty', 'Gamma'], 
+              ['Medium', (6, 12), 'Empty', 'Delta'],
+              ['Big', (18, 24), 'Occupied', 'Epsilon'],
+              ['Big', (11, 18), 'Empty', 'Eta'],
+              ['Medium', (7, 20), 'Empty', 'Theta'],
+              ['Small', (10, 14), 'Occupied', 'Iota'],
+              ['Medium', (0, 9), 'Empty', 'Kappa'],
+              ['Big', (14, 19), 'Empty', 'Lambda']]
 
-ship_schedule_1 = [(1, (19, 20), 'Ship 1'), 
-                   (3, (2, 3), 'Ship 2'), 
-                   (2, (2, 5), 'Ship 3'),
-                   (1, (13, 14), 'Ship 4'),
-                   (2, (7, 10), 'Ship 5'),
-                   (3, (15, 18), 'Ship 6')]
+ship_schedule_1 = [('Small', (19, 20), 'Ship 1'), 
+                   ('Big', (2, 3), 'Ship 2'), 
+                   ('Medium', (2, 5), 'Ship 3'),
+                   ('Small', (13, 14), 'Ship 4'),
+                   ('Medium', (7, 10), 'Ship 5'),
+                   ('Big', (15, 18), 'Ship 6')]
 
 
 def bay_selection(ship_schedule, ship_bay):
